28e6ccac766d7c606308bfd67866405681401232
alex <alex@alexmoica.com>
2020-07-11 01:39:45 -0400

    added modals to project images

---
+++ js/projectModals.js
@@ -0,0 +1,31 @@
+$('img[hasModal]').addClass('img-enlargable').click(function(){
+        $(".overlay").show();
+        var src = $(this).attr('src');
+        var modal;
+        function removeModal(){ modal.remove(); $('body').off('keyup.modal-close'); }
+        modal = $('<div class="modal-animate">').css({
+            background: 'url('+src+') no-repeat center',
+            backgroundSize: 'contain',
+            width:'100%', height:'60%',
+            position:'fixed',
+            zIndex:'10',
+            top:'20%', left:'0',
+            cursor: 'pointer'
+
+            }).click(function(){
+            $(".overlay").hide();
+            removeModal();
+            }).appendTo('body');
+
+            $(".overlay").click(function(){
+                $(".overlay").hide();
+                removeModal();
+                });
+
+            //handling ESC
+            $('body').on('keyup.modal-close', function(e){
+            if(e.key==='Escape'){ 
+                $(".overlay").hide();
+                removeModal(); } 
+        });
+    });
