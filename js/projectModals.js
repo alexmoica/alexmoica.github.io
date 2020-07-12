9f0ffe7bfff7ae5d699014dda383a685c1cf2a6f
alex <alex@alexmoica.com>
2020-07-12 04:31:04 -0400

    fixed article bubbling, finalised projects

--- js/projectModals.js
+++ js/projectModals.js
@@ -1,31 +1,41 @@
-$('img[hasModal]').addClass('img-enlargable').click(function(){
-        $(".overlay").show();
-        var src = $(this).attr('src');
-        var modal;
-        function removeModal(){ modal.remove(); $('body').off('keyup.modal-close'); }
-        modal = $('<div class="modal-animate">').css({
-            background: 'url('+src+') no-repeat center',
-            backgroundSize: 'contain',
-            width:'100%', height:'60%',
-            position:'fixed',
-            zIndex:'10',
-            top:'20%', left:'0',
-            cursor: 'pointer'
+$('img[hasModal]').addClass('img-enlargable').click(function() {
+	$(".overlay").show();
+	var src = $(this).attr('src');
+	var modal;
 
-            }).click(function(){
-            $(".overlay").hide();
-            removeModal();
-            }).appendTo('body');
+	function removeModal() {
+		modal.remove();
+		$('body').off('keyup.modal-close');
+	}
+	modal = $('<div class="modal-animate">').css({
+		background: 'url(' + src + ') no-repeat center',
+		backgroundSize: 'contain',
+		width: '100%',
+		height: '60%',
+		position: 'fixed',
+		zIndex: '10',
+		top: '20%',
+		left: '0',
+		cursor: 'pointer'
 
-            $(".overlay").click(function(){
-                $(".overlay").hide();
-                removeModal();
-                });
+	}).click(function() {
+		$(".overlay").hide();
+		removeModal();
+		event.stopPropagation();
+	}).appendTo('body');
 
-            //handling ESC
-            $('body').on('keyup.modal-close', function(e){
-            if(e.key==='Escape'){ 
-                $(".overlay").hide();
-                removeModal(); } 
-        });
-    });
+	$(".overlay").click(function() {
+		$(".overlay").hide();
+		removeModal();
+		event.stopPropagation();
+	});
+
+	//handling ESC
+	$('body').on('keyup.modal-close', function(e) {
+		if (e.key === 'Escape') {
+			$(".overlay").hide();
+			removeModal();
+			event.stopPropagation();
+		}
+	});
+});
